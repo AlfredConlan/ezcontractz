64ca5f3e706922b44e06fa316ac0ea45b7c99494
Al Conlan <oconellan@gmail.com>
2021-11-20 20:41:52 -0600

    created database and API calls for user endpoints

---
+++ migrations/20211121013034-create-tasks.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      taskName: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      assignedContractor: {
+        type: Sequelize.STRING
+      },
+      scheduled: {
+        type: Sequelize.BOOLEAN
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      maxBudet: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tasks');
+  }
+};
---
+++ models/tasks.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tasks extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Tasks.init({
+    taskName: DataTypes.STRING,
+    category: DataTypes.STRING,
+    description: DataTypes.STRING,
+    assignedContractor: DataTypes.STRING,
+    scheduled: DataTypes.BOOLEAN,
+    date: DataTypes.DATE,
+    maxBudet: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Tasks',
+  });
+  return Tasks;
+};
