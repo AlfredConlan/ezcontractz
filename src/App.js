3703c79d0215d7888aa15163dffa9abeeeb4d777
Al Conlan <oconellan@gmail.com>
2021-11-26 12:50:27 -0600

    got navbar working with router in app.js

--- src/App.js
+++ src/App.js
@@ -11,19 +11,15 @@ import LoginRegistration from "./Components/LoginRegistration";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <p>
-          <h1>Welcome to EZContractz</h1>
-        </p>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<TaskList />} />
-            <Route path="/admin" element={<AdminDash />} />
-            <Route path="/login" element={<LoginRegistration />} />
-          </Routes>
-        </BrowserRouter>
-      </header>
+      <BrowserRouter>
+        <Navbar />{" "}
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/tasks" element={<TaskList />} />
+          <Route path="/admin" element={<AdminDash />} />
+          <Route path="/login" element={<LoginRegistration />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
