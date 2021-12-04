31402ccb6d8148ebe8d5c63de77e8cd4c3ff2c09
Al Conlan <oconellan@gmail.com>
2021-12-04 11:39:34 -0600

    added some styling and responsiveness to tasks and admin

--- src/Components/AdminTable.js
+++ src/Components/AdminTable.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useMemo, useRef } from "react";
 import { useTable } from "react-table";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Trash, Pencil} from 'react-bootstrap-icons';
-
+import { Trash, Pencil } from "react-bootstrap-icons";
 
 const AdminTable = (props) => {
   const [users, setUsers] = useState([]);
@@ -20,23 +19,23 @@ const AdminTable = (props) => {
     setSearchUsers(searchUsers);
   };
 
-    // Fetching users from database 
+  // Fetching users from database
   const retrieveUsers = () => {
     fetch("http://localhost:3001/users")
-    .then(resp => resp.json())
-    .then(resp => {
-        setUsers(resp)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setUsers(resp);
         console.log(users);
-    })
+      });
   };
 
   const refreshList = () => {
     retrieveUsers();
   };
 
-
   const findByTitle = () => {
-    users.findByTitle(users)
+    users
+      .findByTitle(users)
       .then((response) => {
         setUsers(response.data);
       })
@@ -52,10 +51,11 @@ const AdminTable = (props) => {
   };
 
   const deleteUsers = (rowIndex) => {
-    const userName = usersRef.current[rowIndex].user_name; 
-    users.remove(userName)
+    const userName = usersRef.current[rowIndex].user_name;
+    users
+      .remove(userName)
       .then((response) => {
-        props.history.push("/users/delete/"+ userName);
+        props.history.push("/users/delete/" + userName);
         let newUsers = [...usersRef.current];
         newUsers.splice(rowIndex, 1);
 
@@ -97,9 +97,9 @@ const AdminTable = (props) => {
             <div>
               <span onClick={() => openUsers(rowIdx)}>
                 <Pencil className="far fa-edit action mr-2" />
-              </span> 
+              </span>
               <span onClick={() => deleteUsers(rowIdx)}>
-                <Trash className="bi bi-trash"/>
+                <Trash className="bi bi-trash" />
               </span>
             </div>
           );
@@ -109,70 +109,49 @@ const AdminTable = (props) => {
     []
   );
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
     columns,
     data: users,
   });
 
   return (
-    <div className="list row">
-      <div className="col-md-8">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search by title"
-            value={searchUsers}
-            onChange={onChangeSearchUsers}
-          />
-          <div className="input-group-append">
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              onClick={findByTitle}
-            >
-              Search
-            </button>
+    <div className="container">
+      <div className="list row">
+        <div className="col-md-8 container">
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Search by title" value={searchUsers} onChange={onChangeSearchUsers} />
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary btnOrange" type="button" onClick={findByTitle}>
+                Search
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="col-md-12 list">
-        <table
-          className="table table-striped table-bordered"
-          {...getTableProps()}
-        >
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row, i) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                    );
-                  })}
+        <div className="col-md-12 list">
+          <table className="table table-striped table-bordered" {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                  ))}
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row, i) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
